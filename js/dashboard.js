146d45b0af0f82b4ae0c0336160aa19df0d59759
Leo Lou <leolou@Leos-MacBook-Pro-3.local>
2018-04-14 21:33:18 -0700

    icons added

--- js/dashboard.js
+++ js/dashboard.js
@@ -23,6 +23,16 @@ controls.addEventListener('click', function expand(ev){
     }
 });
 
+//Navigation
+navElements[0].addEventListener('click', function(ev){
+    location.href = "../index.html";
+});
+navElements[2].addEventListener('click',function(ev){
+    location.href = "../html/portfolio.html";
+});
+
+
+// primary functions
 function contract(){
     dashboard.style.width = "100px";
     navigation.style.width = "100px";
@@ -32,11 +42,21 @@ function contract(){
     logo.style.width = "50px";
     logo.style.height = "50px";
     logo.style.transition = "all 0.5s";
-    logout_button.style.marginLeft = "0";
     logout_button.style.width = "70px";
+    logout_button.style.padding = "0";
+    logout_button.style.pointerEvents = "none";
+    logout_button.innerHTML = "";
+    logout_button.style.marginLeft = "0";
+    logout_button.style.opacity = "0";
     logout_button.style.transition = "all 0.5s";
     display.style.width = "90vw";
     display.style.transition = "all 0.5s";
+
+    //icons
+    navElements[0].innerHTML = "<img width='50' height='56' src='./imgs/Overview.png' alt='overview icon'/>";
+    navElements[1].innerHTML = "<img width='50' height='56' src='./imgs/Trade.png' alt='trade icon'/>";
+    navElements[2].innerHTML = "<img width='50' height='56' src='./imgs/Portfolio.png' alt='portfolio icon'/>";
+    navElements[3].innerHTML = "<img width='50' height='56' src='./imgs/Settings.png' alt='settings icon'/>";
 }
 
 function Dexpand(){
@@ -48,9 +68,18 @@ function Dexpand(){
     logo.style.width = "100px";
     logo.style.height = "100px";
     logo.style.transition = "all 0.5s";
-    logout_button.style.marginLeft = "35px";
     logout_button.style.width = "100px";
-    logout_button.style.transition = "all 0.5s";
+    logout_button.style.marginLeft = "35px";
+    logout_button.style.padding = "14px";
+    logout_button.style.pointerEvents = "auto";
+    logout_button.innerHTML = "Logout";
+    logout_button.style.opacity = "1";
     display.style.width = "80vw";
     display.style.transition = "all 0.5s";
+
+    //icons vanish
+    navElements[0].innerHTML = "Overview";
+    navElements[1].innerHTML = "Trade";
+    navElements[2].innerHTML = "Portfolio";
+    navElements[3].innerHTML = "Settings";
 }
