1ec034aecdc53825e05bcaba8772c8d847057ae0
unknown <jiafrank98@gmail.com>
2018-04-19 22:08:09 -0700

    dashbaord changes

--- js/dashboard.js
+++ js/dashboard.js
@@ -13,11 +13,11 @@ var dash_clicks = 0;
 controls.addEventListener('click', function expand(ev){
     dash_clicks++;
     if (dash_clicks == 1){
-        controls.innerHTML = "&gt;";
+        controls.innerHTML = "<img class='icon_images' src='./imgs/keyboard-right-arrow-button.png'>";
         // dashboard and navigation and elements contract
         contract();
     } else if (dash_clicks == 2){
-        controls.innerHTML = "&lt;";
+        controls.innerHTML = "<img class='icon_images' src='./imgs/left-arrow-key.png'>";        
         dash_clicks = 0;
         // dashboard expand
         Dexpand();
@@ -34,37 +34,27 @@ navElements[1].addEventListener('click', function(ev){
 navElements[2].addEventListener('click',function(ev){
     location.href = "../bullcryp-dash-mvp/html/portfolio.html";
 });
-navElements[3].addEventListener('click', function(ev){
-    location.href = "../bullcryp-dash-mvp/html/settings.html";
-});
 
 
 // primary functions
 function contract(){
-    dashboard.style.width = "100px";
-    navigation.style.width = "100px";
+    dashboard.style.width = "125px";
+    navigation.style.width = "125px";
     navigation.style.textAlign = "center";
     dashboard.style.transition = "all 0.5s";
     navigation.style.transition = "all 0.5s";
     logo.style.marginLeft = "25px";
-    logo.style.width = "50px";
-    logo.style.height = "50px";
+    logo.style.width = "75px";
+    logo.style.height = "75px";
     logo.style.transition = "all 0.5s";
-    logout_button.style.width = "70px";
-    logout_button.style.padding = "0";
-    logout_button.style.pointerEvents = "none";
-    logout_button.innerHTML = "";
-    logout_button.style.marginLeft = "0";
-    logout_button.style.opacity = "0";
-    logout_button.style.transition = "all 0.5s";
     display.style.marginLeft = "100px";
     display.style.transition = "all 0.5s";
 
     //icons
-    navElements[0].innerHTML = "<img width='50' height='50' src='./imgs/Overview_icon.png' alt='overview icon'/>";
-    navElements[1].innerHTML = "<img width='50' height='50' src='./imgs/Trade_icon.png' alt='trade icon'/>";
-    navElements[2].innerHTML = "<img width='50' height='50' src='./imgs/Portfolio_icon.png' alt='portfolio icon'/>";
-    navElements[3].innerHTML = "<img width='50' height='50' src='./imgs/Settings_icon.png' alt='settings icon'/>";
+    navElements[0].innerHTML = "<img width='35' height='35' src='./imgs/Overview_icon.png' alt='overview icon'/>";
+    navElements[1].innerHTML = "<img width='35' height='35' src='./imgs/Trade_icon.png' alt='trade icon'/>";
+    navElements[2].innerHTML = "<img width='35' height='35' src='./imgs/Settings_icon.png' alt='settings icon'/>";
+    navElements[3].innerHTML = "<img width='35' height='35' src='./imgs/power-button.png' alt='logout icon'/>";    
 }
 
 function Dexpand(){
@@ -77,18 +67,12 @@ function Dexpand(){
     logo.style.width = "100px";
     logo.style.height = "100px";
     logo.style.transition = "all 0.5s";
-    logout_button.style.width = "100px";
-    logout_button.style.marginLeft = "35px";
-    logout_button.style.padding = "14px";
-    logout_button.style.pointerEvents = "auto";
-    logout_button.innerHTML = "Logout";
-    logout_button.style.opacity = "1";
     display.style.marginLeft = "200px";
     display.style.transition = "all 0.5s";
 
     //icons vanish
-    navElements[0].innerHTML = "<img class='icon_images' src='./imgs/Overview_icon.png' alt='overview'>Overview";
-    navElements[1].innerHTML = "<img class='icon_images' src='./imgs/Trade_icon.png' alt='trade'>Trade";
-    navElements[2].innerHTML = "<img class='icon_images' src='./imgs/Portfolio_icon.png' alt='portfolio'>Portfolio";
-    navElements[3].innerHTML = "<img class='icon_images' src='./imgs/Settings_icon.png' alt='settings'>Settings";
+    navElements[0].innerHTML = "<img class='icon_images' src='./imgs/Overview_icon.png' alt='overview'><p>Overview</p>";
+    navElements[1].innerHTML = "<img class='icon_images' src='./imgs/Trade_icon.png' alt='trade'><p>Overview</p>";
+    navElements[2].innerHTML = "<img class='icon_images' src='./imgs/Settings_icon.png' alt='settings'><p>Overview</p>";
+    navElements[3].innerHTML = "<img class='icon_images' src='./imgs/power-button.png' alt='logout'><p>Overview</p>";    
 }
